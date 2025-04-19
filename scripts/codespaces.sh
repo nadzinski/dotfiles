fc5a34ba34aac457f3965dbfd4ee00a93ab121db
Nadia Wadzinski <nadia.wadzinski@rover.com>
2025-04-19 10:30:25 -0700

    Claude code env vars

--- scripts/codespaces.sh
+++ scripts/codespaces.sh
@@ -1,3 +1,8 @@
+export CLAUDE_CODE_USE_BEDROCK=1
+export AWS_PROFILE=dev
+export AWS_REGION=us-west-2
+export ANTHROPIC_MODEL=us.anthropic.claude-3-7-sonnet-20250219-v1:0
+
 function write_aws_saml_credentials {
     if [[ ! -z "${ROVER_AWS_SAML_HELPER_CREDENTIALS:-}" ]]; then
         mkdir -p $HOME/.aws
