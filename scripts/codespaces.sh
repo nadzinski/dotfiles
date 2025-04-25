97238f62b938e298e3fc7c49e2b3852078722c12
Nadia Wadzinski <nadia.wadzinski@rover.com>
2025-04-25 11:38:55 -0700

    neovim install

--- scripts/codespaces.sh
+++ scripts/codespaces.sh
@@ -1,3 +1,12 @@
+# Install neovim. We must do this manually as the latest version of the package registry is outdated.
+curl -fsLSO https://github.com/neovim/neovim/releases/download/stable/nvim-linux-x86_64.appimage
+chmod 755 nvim-linux-x86_64.appimage
+./nvim-linux-x86_64.appimage --appimage-extract
+mv squashfs-root $HOME/.nvim
+rm nvim-linux-x86_64.appimage
+mkdir -p $HOME/.local/bin
+ln -sf "$HOME/.nvim/usr/bin/nvim" "$HOME/.local/bin"
+
 npm install -g @anthropic-ai/claude-code
 
 function write_aws_saml_credentials {
