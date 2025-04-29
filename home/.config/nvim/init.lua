d0cf3ea662ba2f0b4a33f2824a3b18dea70275db
Nadia Wadzinski <nadia.wadzinski@rover.com>
2025-04-28 18:45:29 -0700

    don't notify on plugin updates

--- home/.config/nvim/init.lua
+++ home/.config/nvim/init.lua
@@ -169,7 +169,7 @@ require("lazy").setup({
   -- colorscheme that will be used when installing plugins.
   install = { colorscheme = { "tokyonight" } },
   -- automatically check for plugin updates
-  checker = { enabled = true },
+  checker = { enabled = true , notify = false},
 })
 
 require('fzf-lua').setup({
