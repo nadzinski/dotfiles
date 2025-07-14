ac19b91c2facb29bb4c851dfefd0ec3d5d99237a
Nadia Wadzinski <nadia.wadzinski@rover.com>
2025-07-14 13:10:15 -0700

    remove claude code config

--- scripts/codespaces.sh
+++ scripts/codespaces.sh
@@ -8,14 +8,12 @@ mkdir -p $HOME/.local/bin
 ln -sf "$HOME/.nvim/usr/bin/nvim" "$HOME/.local/bin"
 
 sudo apt update && sudo apt install silversearcher-ag
-
-npm install -g @anthropic-ai/claude-code
-
-function write_aws_saml_credentials {
-    if [[ ! -z "${ROVER_AWS_SAML_HELPER_CREDENTIALS:-}" ]]; then
-        mkdir -p $HOME/.aws
-        echo $ROVER_AWS_SAML_HELPER_CREDENTIALS | base64 -d > $HOME/.aws/credentials
-    fi
-}
-# Write credentials on load if possible
-write_aws_saml_credentials
+#
+# function write_aws_saml_credentials {
+#     if [[ ! -z "${ROVER_AWS_SAML_HELPER_CREDENTIALS:-}" ]]; then
+#         mkdir -p $HOME/.aws
+#         echo $ROVER_AWS_SAML_HELPER_CREDENTIALS | base64 -d > $HOME/.aws/credentials
+#     fi
+# }
+# # Write credentials on load if possible
+# write_aws_saml_credentials
